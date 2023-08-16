c75d239adba2520791a21dae4067c73df821dbf1
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-08-17 06:46:29 +0900

    modified ProductInfoPage

--- src/main/java/com/qa/opencart/pages/ProductInfoPage.java
+++ src/main/java/com/qa/opencart/pages/ProductInfoPage.java
@@ -1,7 +1,12 @@
 package com.qa.opencart.pages;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.qa.opencart.constants.AppConstants;
 import com.qa.opencart.utils.ElementUtil;
@@ -15,6 +20,8 @@ public class ProductInfoPage {
 	private By productImages =By.cssSelector("ul.thumbnails img");
 	private By quantity = By.name("quantity");
 	private By addToCartBtn	= By.linkText("Add to Cart");
+	private By ProductMetaData = By.xpath("(//div[@id='content']//ul[@class='list-unstyled'])[1]/li");
+	private By ProductPriceData = By.xpath("(//div[@id='content']//ul[@class='list-unstyled'])[2]/li");
 
 	public ProductInfoPage(WebDriver driver) {
 		this.driver=driver;
@@ -30,5 +37,32 @@ public class ProductInfoPage {
 		System.out.println("total product images for :" +getProductHeaderText() + "===>" + actProductImagesCount);
 		return actProductImagesCount;
 	}
- 
+	
+//	Brand: Apple
+//	Product Code: Product 18
+//	Reward Points: 800
+//	Availability: In Stock
+	public Map<String, String> getProductMetaData() {
+		List<WebElement> metaList = eleUtil.waitForElementsVisible(ProductMetaData, AppConstants.MEDIUM_TIME_OUT);
+		Map<String, String> metaMap=new HashMap<String, String>();
+		for(WebElement e:metaList) {
+			String metaText = e.getText();
+			String key = metaText.split(":")[0].trim();
+			String value = metaText.split(":")[1].trim();
+			metaMap.put(key, value);
+		}
+		return metaMap;
+	}
+//	$2,000.00
+//	Ex Tax: $2,000.00
+	public Map<String, String> getProductPriceData() {
+		List<WebElement> priceList = eleUtil.waitForElementsVisible(ProductPriceData, AppConstants.MEDIUM_TIME_OUT);
+		Map<String,String> priceMap=new HashMap<String,String>();
+		String actPrice = priceList.get(0).getText().trim();
+		String exTax = priceList.get(1).getText().split(":")[0].trim();
+		String exTaxValue = priceList.get(1).getText().split(":")[1].trim();
+		priceMap.put("price", actPrice);
+		priceMap.put(exTax, exTaxValue);
+		return priceMap;
+	}
 }
