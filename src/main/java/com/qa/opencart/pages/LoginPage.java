a167ad36e7a38a8b5faa72444d5769865f233eef
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-09-01 14:01:46 +0900

    added allure annotations

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import com.qa.opencart.constants.AppConstants;
 import com.qa.opencart.utils.ElementUtil;
 
+import io.qameta.allure.Step;
+
 public class LoginPage {
 	
 		private WebDriver driver;
@@ -25,22 +27,27 @@ public class LoginPage {
 		eleUtil =new ElementUtil(driver);
 	}
 	
-	//3. Public Page actions/methods 
+	//3. Public Page actions/methods
+	@Step(".......getting login page title......")
 	public String getLoginPageTitle() {
 		String title = eleUtil.waitForTitleIs(AppConstants.LOGIN_PAGE_TITLE, AppConstants.SHORT_TIME_OUT);
 		System.out.println("Login page title is: " +title);
 		return title;
 	}
 	
+	@Step(".......getting login page url......")
 	public String getLoginPageURL() {
 		String url = eleUtil.waitForURLContains(AppConstants.LOGIN_PAGE_URL_FRACTION, AppConstants.SHORT_TIME_OUT);
 		System.out.println("Login page url is: " + url);
 		return url;
 	}
+	
+	@Step(".......is forgot pwd link exists or not......")
 	public boolean isForgotPwdLinkExist() {
 		return eleUtil.waitForElementVisible(forgotpwdLink, AppConstants.MEDIUM_TIME_OUT).isDisplayed();
 	}
 	
+	@Step("login to app with username {0} and password {1}")
 	public AccountsPage doLogin(String username, String pwd) {
 		System.out.println("App credentials are: " + username + ":" +pwd);
 		eleUtil.waitForElementVisible(emailID, AppConstants.MEDIUM_TIME_OUT).sendKeys(username);
--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -6,25 +6,42 @@ import org.testng.annotations.Test;
 import com.qa.opencart.base.BaseTest;
 import com.qa.opencart.constants.AppConstants;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+
+@Epic("EPIC - 100: Design of the login page for open cart app")
+@Story("User Story - 200: implement login page features for open cart app")
 public class LoginPageTest extends BaseTest{
 	
+	@Description("login page title test.....")
+	@Severity(SeverityLevel.NORMAL)
 	@Test(priority=1)
 	public void loginPageTitleTest() {
 		String actloginPageTitle = loginpage.getLoginPageTitle();
 		Assert.assertEquals(actloginPageTitle, AppConstants.LOGIN_PAGE_TITLE);
 	}
 	
+	@Description("login page url test.....")
+	@Severity(SeverityLevel.NORMAL)
 	@Test(priority=2)
 	public void getLoginPageURLTest() {
 		String actURL = loginpage.getLoginPageURL();
 		Assert.assertTrue(actURL.contains(AppConstants.LOGIN_PAGE_URL_FRACTION));
 	}
 	
+	@Description("check forgot pwd link on login page.....")
+	@Severity(SeverityLevel.NORMAL)
 	@Test(priority=3)
 	public void isForgotPwdLinkExistTest() {
 		Assert.assertTrue(loginpage.isForgotPwdLinkExist());
 	}
 	
+	@Description("Check whether user able to login to open cart app with valid credentials.....")
+	@Severity(SeverityLevel.NORMAL)
 	@Test(priority=4)
 	public void loginTest() {
 		accPage=loginpage.doLogin(prop.getProperty("username"),prop.getProperty("password"));
