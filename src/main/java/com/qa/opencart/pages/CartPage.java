dcf6f1d98e2cc2e0ed45dc69958e8616c438f868
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-09-05 13:26:40 +0900

    updated cartPage as per NaveenTL review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
-	String product="macbook";
+	By product=By.id("productId");
+	
+	public void clickproduct() {
+		System.out.println("click on macbook");
+	}
 	
 }
