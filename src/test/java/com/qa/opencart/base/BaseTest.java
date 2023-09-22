8e63d076fe1003a9001b4fb67bdbc498013fbaa8
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-09-22 13:32:21 +0900

    added parameter in testng.xml

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.DriverFactory;
@@ -26,11 +27,14 @@ public class BaseTest {
 	
 	protected SoftAssert softAsset;
 	
-		
+	@Parameters({"browser"})
 	@BeforeTest()
-	public void setUp() {
+	public void setUp(String browserName) {
 		df=new DriverFactory();
 		prop=df.initProp();
+		if(browserName!=null) {//it means passing the browser from testng.xml
+			prop.setProperty("browser", browserName);
+		}
 		driver=df.initDriver(prop);
 		loginpage = new LoginPage(driver);
 		registerPage = new RegisterPage(driver);
