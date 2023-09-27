3e42e58d2f511fe6f9a2f4fc250d4e2af7251b9a
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-09-27 12:03:27 +0900

    modified OptionsManager

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -37,8 +37,8 @@ public class OptionsManager {
 			Map<String, Object> selenoidOptions = new HashMap<>();
 			selenoidOptions.put("screenResolution", "1280x1024x24");
 			selenoidOptions.put("enableVNC", true);
-			//selenoidOptions.put("name", prop.getProperty("testname"));
-			co.setCapability("selenod:options", selenoidOptions);
+			selenoidOptions.put("name", prop.getProperty("testname"));
+			co.setCapability("selenoid:options", selenoidOptions);
 			
 		}
 		return co;
